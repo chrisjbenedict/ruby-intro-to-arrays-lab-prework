145a795e76ef2f8b92c1c8e91b557f0c9dcb0dd1
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 18:03:15 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,15 @@ end
 def array_with_two_elements
   return ["Snake", "Pothos"]
 end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
